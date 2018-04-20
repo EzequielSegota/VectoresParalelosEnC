399cc04d3ed768365dc6321ce2c96d30846a27b7
EzequielSegota <36193787+EzequielSegota@users.noreply.github.com>
2018-04-20 12:01:18 -0300

    Add files via upload

--- VectoresParalelos/main.c
+++ VectoresParalelos/main.c
@@ -13,6 +13,7 @@ float sacarPromedio(int,int);//recordar castear
 void mostrarAlumnos (char [][50],int [],int [],float [],int [],int []);
 void cargarAlumnos (char [][50],int [],int [],float [],int [],int []);
 int buscarLugar (int [], int []);
+void OrdenamientoNombre(char [][50],int [],int [],float [],int [],int);
 
 int main()
 {
@@ -21,10 +22,57 @@ int main()
     int notasDos[TAMANIO]={4,8,7,8,3};
     float promedios[TAMANIO]={4,9.5,4.5,8.5,2.5};
     int legajo[TAMANIO]={111,222,333,444,555};
+    int opcion;
+    char continuar='s';
+
+
+    while(continuar=='s')
+    {
+        printf("\nPresione 1 para ingresar nombre\n");
+        printf("\nPresione 2 para ingresar nota uno\n");
+        printf("\nPresione 3 para ingresar nota dos\n");
+        printf("\nPresione 4 para ingresar legajo\n");
+        printf("\nPresione 5 para mostrar alumnos\n");
+        printf("\nPresione 6 para ordenar por nombre\n");
+        printf("\nPresione 7 para salir\n");
+
+
+        scanf("%d",&opcion);
+
+        switch(opcion)
+        {
+        case 1:
+            printf("Ingrese nombre: ");
+            fflush(stdin);
+            gets(nombre);
+            break;
+        case 2:
+            printf("Ingrese nota uno: ");
+
+            scanf("%d",&notasUno);
+            break;
+        case 3:
+            printf("Ingrese nota dos: ");
+            scanf("%d",&notasDos);
+            break;
+        case 4:
+            printf("Ingrese legajo: ");
+            scanf("%d",&legajo);
+            break;
+        case 5:
+            mostrarAlumnos(nombre,notasUno,notasDos,promedios,legajo,TAMANIO);
+            break;
+        case 6:
+            OrdenamientoNombre(nombre,notasUno,notasDos,promedios,legajo,TAMANIO);
+            break;
+        case 7:
+            continuar='n';
+            break;
+        }
+
+    }
 
     cargarAlumnos(nombre,notasUno,notasDos,promedios,legajo,TAMANIO);
-
-    mostrarAlumnos(nombre,notasUno,notasDos,promedios,legajo,TAMANIO);
     return 0;
 }
 
@@ -34,7 +82,7 @@ void mostrarAlumnos (char nombre[][50],int primeraNota[],int segundaNota[],float
 
     for(i=0;i<largo;i++)
     {
-        printf("\nl:%d n:%s n1:%d n2:%d p:%.2f", legajo[i],nombre[i],primeraNota[i],segundaNota[i],promedio[i]);
+        printf("\nL:%d N:%s N1:%d N2:%d P:%.2f", legajo[i],nombre[i],primeraNota[i],segundaNota[i],promedio[i]);
     }
 }
 
@@ -62,4 +110,25 @@ int buscarLugar (int legajo[], int largo[])
     }
 
     return indiceRetornado;
+}
+
+OrdenamientoNombre(char nombre[][50],int primeraNota[],int segundaNota[],float promedio[],int legajo[],int largo)
+{
+    int i,j,comparacion;
+    float aux;
+    char auxChar[largo];
+
+    for(i=0;i<largo-1;i++)
+    {
+        for(j=i+1;j<largo;j++)
+        {
+            comparacion=strcmp(nombre[i], nombre[j]);
+            if(comparacion==1)
+            {
+                strcpy(auxChar, nombre[j]);
+                strcpy(nombre[j], nombre[i]);
+                strcpy(nombre[i], auxChar);
+            }
+        }
+    }
 }
